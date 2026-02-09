c63929c5f4771c9942dc88e76f78c5fb33699cc6
sahilaathia <athiasahila@gmail.com>
2026-02-09 15:47:36 +0700

    change def divide to branch_divide (2)

--- LatihanAdpro1.py
+++ LatihanAdpro1.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     print("Addition:", calc.add(10, 5))
     print("Subtraction:", calc.subtract(10, 5))
     print("Multiplication:", calc.multiply(10, 5))
-    print("Division:", calc.divide(10, 5))
+    print("Division:", calc.branch_divide(10, 5))
