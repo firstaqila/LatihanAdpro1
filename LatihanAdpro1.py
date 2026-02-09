1714f6968b414340323a696e29994a037a756ffa
firstaqila <saffana.firsta@gmail.com>
2026-02-09 14:48:42 +0700

    change multiply to branch_multiply

--- LatihanAdpro1.py
+++ LatihanAdpro1.py
@@ -5,7 +5,7 @@ class Calculator:
     def subtract(self, a, b):
         return a - b
 
-    def multiply(self, a, b):
+    def branch_multiply(self, a, b):
         return a * b
 
     def divide(self, a, b):
@@ -16,5 +16,5 @@ if __name__ == "__main__":
 
     print("Addition:", calc.add(10, 5))
     print("Subtraction:", calc.subtract(10, 5))
-    print("Multiplication:", calc.multiply(10, 5))
+    print("Multiplication:", calc.branch_multiply(10, 5))
     print("Division:", calc.divide(10, 5))
